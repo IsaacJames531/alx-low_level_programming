17829f068a0fd950e1ab072608568737615d45e2
Isaac <isaacojames@gmail.com>
2022-09-09 04:27:33 -0700

    ll

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,12 +3,12 @@
 /**
 * main - print the string in the put function
 *
-* Description: using the main the function
+* Description: using the main function
 * this program prints "Programming is like building a multilingual puzzle
 * Return: 0
 */
 int main(void)
 {
-        printf("with proper grammer, but the outcome is a piece of art,\n");
-        return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
