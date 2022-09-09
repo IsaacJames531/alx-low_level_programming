57a6aee7d0b89f5295bdcf67e77df80035fa148b
Isaac <isaacojames@gmail.com>
2022-09-09 02:34:35 -0700

    hey

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,6 @@
-include <stdio.h>
-
-
-
-/**
-
- * main - Entry point
-
- *
-
- * Return: Always 0 (Success)
-
- */
-
+#include <stdio.h>
 int main(void)
-
 {
-
-	puts("\"Programming is like building a multilingual puzzle");
-
+	puts("\"Programmming is like building a multilingual puzzle");
 	return (0);
-
 }
