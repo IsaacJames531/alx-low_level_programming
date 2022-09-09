732dbbad67e691f07c964df873006eb05752df76
Isaac <isaacojames@gmail.com>
2022-09-09 04:11:51 -0700

    hmm

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include  <stdio.h>
+
+#include <stdio.h>
+
+/**
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints "with proper grammar, but the outcome is a piece of art,
+* Return: 0
+*/
+int main(void)
+{
+	printf("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
