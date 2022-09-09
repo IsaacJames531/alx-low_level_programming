b9c0c8d86146b29c6e20b3d4a69ecacf56590069
Isaac <isaacojames@gmail.com>
2022-09-09 10:39:43 -0700

    working

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  * main - Prints number for base 10
+  *
+  * Return: Always (Success)
+  */
+int main(void)
+{
+	int a;
+
+	for (a = '0'; c <= '10'; a++)
+	{
+		print (a)
+	}
+
+	return (0);
+}
