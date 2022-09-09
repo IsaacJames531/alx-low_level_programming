180123e320b2a25bd95d7e34159fff2b33a43eb4
Isaac <isaacojames@gmail.com>
2022-09-09 11:04:46 -0700

    kk

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  * main - Prints number for base 10
+  *
+  * Return: Always (Success)
+  */
+int main(void)
+{
+	char a;
+
+	for (a = '0'; a < '11'; a++)
+	{
+		putchar(a)
+	}
+	
+	putchar('\a');
+
+	return (0);
+}
