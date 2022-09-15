97630e1cde4b3bfa9f22f912a29626e405bcfaa6
IsaacJames531 <105295945+IsaacJames531@users.noreply.github.com>
2022-09-15 17:15:48 +0100

    who

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,16 +4,15 @@
 */
 
 void print_alphabet_x10(void)
-{	
-	int a;
+{	int a;
 	int j;
 
     for (a = 0; a < 10; a++)	
 	{
 		for (j = 'a'; j <= 'z'; j++)
-	{
+		{
 		_putchar(j);
-	}
+		}
 	_putchar('\n');
 	}
 	
