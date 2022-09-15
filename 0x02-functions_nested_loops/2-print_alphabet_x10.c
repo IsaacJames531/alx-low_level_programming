f95a60226036db05514aac15242862705304f205
IsaacJames531 <105295945+IsaacJames531@users.noreply.github.com>
2022-09-15 17:11:17 +0100

    wo

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,15 +2,17 @@
 /**
 *print_alphabet_x10 -> print the lower case alhapbet
 */
+
 void print_alphabet_x10(void)
-{	int a;
+{	
+	int a;
 	int j;
 
     for (a = 0; a < 10; a++)	
 	{
-	for (j = 'a'; j <= 'z'; j++)
+		for (j = 'a'; j <= 'z'; j++)
 	{
-		_putchar(a);
+		_putchar(j);
 	}
 	_putchar('\n')
 	}
