c6ab02098df9f59a5e2875561725c8b7781204dc
IsaacJames531 <105295945+IsaacJames531@users.noreply.github.com>
2022-09-16 11:44:11 +0100

    jj

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,15 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
-* print_numbers - prints 0-9 with a new line
-* Description: Uses headers to link and a nested loops to achieve goal
-* Return: void. no return.
-*/
+ * print_numbers - a function that prints the numbers, from 0 to 9
+ *
+ * Return: 0-9 followed by newline
+ */
 void print_numbers(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
-	
-	_putchar(i + '0');
-	  
+	for (i = 0; i <= 9; i++)
+		_putchar(i + '0');
 	_putchar('\n');
 }
