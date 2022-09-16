ea3df8fecdf9a616636851de56b586c82cd007a7
IsaacJames531 <105295945+IsaacJames531@users.noreply.github.com>
2022-09-16 09:21:12 +0100

    ee

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -9,9 +9,11 @@
 
 void print_numbers(void)
 {
-	int i = 0;
+	int i;
 
-	for (i = 0; i < 10; i++)
-		_putchar(i + '0');
-	_putchar('\n');
+	for (i = 48; i < 58; i++)
+	{
+		_putchar(n);
+	}
+	_putchar(10);
 }
