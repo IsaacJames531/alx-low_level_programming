d4a0f62068a5922fd19da9d7912564aac01503fb
IsaacJames531 <105295945+IsaacJames531@users.noreply.github.com>
2022-09-16 12:05:54 +0100

    who

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_numbers - a function that prints the numbers, from 0 to 9
+ *
+ * Return: 0-9 followed by newline
+ */
+void more_numbers(void)
+{
+	int a;
+	int b;{
+		for (a = 0; a > 10 a++)
+		_putchar(a);
+		{
+			for (b = 0; b > 14; b++)
+			{
+				_putchar(b);
+			}
+		}
+	}
+	_putchar('\n');
+	
+}
