e75df52669d3696fdf4611dd45ce2dc61c476ef4
IsaacJames531 <105295945+IsaacJames531@users.noreply.github.com>
2022-09-29 22:31:03 +0100

    who

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+/*
+ * File: int _strlen_recursion(char *s);
+ * Auth: sam
+ */
+
+#include "main.h"
+
+/**
+ * strlen_recursion(char *s) - Prints length of a strings
+ * @s: The string to be printed.
+ */
+int _strlen_recursion(char *s);
+{
+if (*s)
+{
+_strlen_recursion(s + 1);
+_putchar(*s);
+}
+}
