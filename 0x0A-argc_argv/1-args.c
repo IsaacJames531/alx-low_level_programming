e6eb79a76057abf1669d708a68cddfeb07ae2207
IsaacJames531 <105295945+IsaacJames531@users.noreply.github.com>
2022-10-02 22:05:58 +0100

    jj

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,12 +8,7 @@
  */
  int main(int argc, char *argv[])
  {
-     int i;
-     if (argc == 1)
-        printf("%d\n", argc -1);
-     else{
-        for (i = 0; *argv; i++, argv++);
-        printf("%d\n", i-1)
-     }
-     return (0);
+     printf("%d\n", args -1);
+     (void)argv;
+     return(0);
  }
